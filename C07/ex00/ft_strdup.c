0cf7ce0b10ade7c8490cd21591f9619cffc0765c
Artur <artur.13.goncalves@gmail.com>
2023-09-23 13:13:27 +0100

    up

--- C07/ex00/ft_strdup.c
+++ C07/ex00/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: Arturg04 <artur.13.goncalves@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 21:28:24 by Arturg04          #+#    #+#             */
-/*   Updated: 2023/09/21 21:41:47 by Arturg04         ###   ########.fr       */
+/*   Updated: 2023/09/23 13:13:04 by Arturg04         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,17 @@ int	ft_strlen(char *str)
 	return (i);
 }
 
-void	ft_strcpy(char *dest, char *src)
-{
-	while (*dest)
-		*src++ = *dest++;
-	*src = 0;
-}
-
 char	*ft_strdup(char *src)
 {
 	char	*dest;
+	int		i;
 
-	dest = malloc(ft_strlen(src));
+	i = 0;
+	dest = malloc(ft_strlen(src) + 1);
 	if (dest == NULL)
 		return (NULL);
-	ft_strcpy(src, dest);
+	while (dest[i])
+		src[i++] = dest[i];
+	dest[i] = 0;
 	return (dest);
 }
