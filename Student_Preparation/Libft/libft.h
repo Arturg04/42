069ec7fcee09cfbd48ed04f425a6c641f08be6a0
Artur <artur.13.goncalves@gmail.com>
2023-09-28 22:54:58 +0100

    up

--- Student_Preparation/Libft/libft.h
+++ Student_Preparation/Libft/libft.h
@@ -1,23 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Arturg04 <artur.13.goncalves@gmail.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/28 22:51:01 by Arturg04          #+#    #+#             */
+/*   Updated: 2023/09/28 22:53:10 by Arturg04         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# define NULL ((void *)0)
 
-void	ft_bzero(void *src, unsigned int n);
+void			ft_bzero(void *restrict src,
+					unsigned int n);
 
-void	*ft_memccpy(void *dest, const void *src, int c, unsigned int n);
+void			*ft_memccpy(void *restrict dest,
+					const void *restrict src,
+					int c,
+					unsigned int n);
 
-void	*ft_memchr(const void *restrict src, int c, unsigned int n);
+void			*ft_memchr(const void *restrict src,
+					int c,
+					unsigned int n);
 
-int		ft_memcmp(const void *restrict str1, const void *restrict str2, unsigned int n);
+int				ft_memcmp(const void *restrict str1,
+					const void *restrict str2,
+					unsigned int n);
 
-void	*ft_memcpy(void *dest, void *src, unsigned int n);
+void			*ft_memcpy(void *restrict dest,
+					void const *restrict src,
+					unsigned int n);
+void			*ft_memmove(void *restrict dest,
+					const void *restrict src,
+					unsigned int n);
 
-void	*ft_memmove(void *restrict dest, const void *restrict src, unsigned int n);
+void			*ft_memset(void *restrict src, int c, unsigned int n);
 
-void	*ft_memset(void *src, int c, unsigned int n);
-
-unsigned int	ft_strlcpy(char *restrict dest, const char *restrict src, unsigned int size);
+unsigned int	ft_strlcpy(char *restrict dest,
+					const char *restrict src,
+					unsigned int size);
 
 unsigned int	ft_strlen(const char *restrict str);
 
