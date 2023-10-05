e093856d0f48c6815e1a5be17041c7408d7c4bab
Artur <artur.13.goncalves@gmail.com>
2023-10-05 23:39:34 +0100

    up

--- Student_Preparation/Libft/libft.h
+++ Student_Preparation/Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: Arturg04 <artur.13.goncalves@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 22:51:01 by Arturg04          #+#    #+#             */
-/*   Updated: 2023/10/05 21:09:26 by Arturg04         ###   ########.fr       */
+/*   Updated: 2023/10/05 22:35:18 by Arturg04         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -134,14 +134,27 @@ void			ft_putnbr_fd(int n,
 
 t_list			*ft_lstnew(void *content);
 
-void			ft_lstadd_front(t_list **lst, t_list *new);
+void			ft_lstadd_front(t_list **lst,
+					t_list *new);
 
 int				ft_lstsize(t_list *lst);
 
 t_list			*ft_lstlast(t_list *lst);
 
-void			ft_lstadd_back(t_list **lst, t_list *new);
+void			ft_lstadd_back(t_list **lst,
+					t_list *new);
 
-void			ft_lstdelone(t_list *lst, void (*del)(void *));
+void			ft_lstdelone(t_list *lst,
+					void (*del)(void *));
+
+void			ft_lstclear(t_list **lst,
+					void (*del)(void*));
+
+void			ft_lstiter(t_list *lst,
+					void (*f)(void *));
+
+t_list			*ft_lstmap(t_list *lst,
+					void *(*f)(void *),
+					void (*del)(void *));
 
 #endif
