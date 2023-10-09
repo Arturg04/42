b2fb03395c68401b2a8bc00394d801f610aa362c
ade-pinh <ade-pinh@student.42porto.com>
2023-10-09 23:23:04 +0100

    up

---
+++ Student_Preparation/f_printf/include/ft_printf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ade-pinh <ade-pinh@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/07 16:44:49 by ade-pinh          #+#    #+#             */
+/*   Updated: 2023/10/09 19:25:56 by ade-pinh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include "../Libft/libft.h"
+
+int	ft_printf(const char *str, ...);
+
+#endif
---
+++ Student_Preparation/f_printf/src/ft_printf.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ade-pinh <ade-pinh@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/07 16:44:55 by ade-pinh          #+#    #+#             */
+/*   Updated: 2023/10/09 20:13:57 by ade-pinh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_params(char	c,va_list list)
+{
+	(void)list;
+	if (c == 'c')
+	{
+		ft_putchar_fd(va_arg(list, char), 1);
+		return (1);
+	}
+	else if (c == 's')
+		return (0);
+	else if (c == 'p')
+		return (0);
+	else if (c == 'd')
+		return (0);
+	else if (c == 'i')
+		return (0);
+	else if (c == 'u')
+		return (0);
+	else if (c == 'x')
+		return (0);
+	else if (c == 'X')
+		return (0);
+	else if (c == '%')
+		return (0);
+	return (0);
+}
+
+int	ft_printf(const char *str, ...)
+{
+	va_list	list;
+	int		size;
+
+	size = 0;
+	if (!str)
+		return (0);
+	va_start(list, str);
+	while (*str)
+	{
+		if ('%' == *str)
+		{
+			size += ft_params(*(++str), list);
+			str++;
+		}
+		else if (*str)
+			ft_putchar_fd(*str++, 1);
+	}
+	va_end(list);
+	return (size);
+}
